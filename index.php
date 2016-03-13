2f8701a44ff1e5fbe83d513c76ba3c71ba82eb00
nass-r <nasser.abraham@gmail.com>
2016-03-13 11:22:32 +1100

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,15 @@
  </head>
  <body>
 <?php
-$url = "http://www.biblegateway.com/usage/votd/rss/votd.rdf?$version_id";
-$response = file_get_contents($url);
-echo $response;
-	}
-?>
+  include('rssclass.php');
+  $feedlist = new rss('http://feeds2.feedburner.com/9lesson');
+  echo $feedlist->display(9,"9lessons");
+ 
+  $feedlist = new rss('http://feeds.feedburner.com/nettuts');
+  echo $feedlist->display(9,"Nettuts");
+ 
+  $feedlist = new rss('http://feeds.labnol.org/labnol');
+  echo $feedlist->display(9,"Labnol");
+  ?> 
  </body>
 </html>
